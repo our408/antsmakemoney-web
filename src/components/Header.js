b9ff0211380a98c321c6df682e6b0b1a22acacea
km <commonjade@naver.com>
2021-07-18 02:58:35 +0900

    layer udpate

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,9 @@ import ants from "../img/ants50.png";
 function Header() {
     return (
         <React.Fragment>
-            <div className="header_wrap">
+            <div className="header-wrap">
                 <div className="intro">개미도 때론 돈을 번다</div>
-                <img className="smallAnt" src={ants}></img>
+                <img className="small-ant" src={ants}></img>
             </div>
             <div className="line"></div>
         </React.Fragment>
